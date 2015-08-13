c94ffc565d3e9d130985bdb292092a4e61567fd5
Andres Tarantini <atarantini@gmail.com>
2015-08-13 02:12:25 -0300

    New --charset argument.

--- dnscomb.py
+++ dnscomb.py
@@ -27,9 +27,10 @@ if __name__ == "__main__":
     parser.add_argument('--max', type=int, help='Maximum length of the domain name', default=3)
     parser.add_argument('--min', type=int, help='Minimum length of the domain name', default=1)
     parser.add_argument('--tld', type=str, help='Top-level domain, default: .com', default=TLD)
+    parser.add_argument('--charset', type=str, help='Charset for the wordlist, default: abcdefghijklmnopqrstuvwxyz', default=string.ascii_lowercase)
     args = parser.parse_args()
 
-    generator = wordlist.Generator(string.ascii_lowercase)
+    generator = wordlist.Generator(args.charset)
     for name in generator.generate(args.min, args.max):
         hostname = MASK.format(
             name=name,
