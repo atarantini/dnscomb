c0d096964e83e40ca8c55f789037750e09986506
Andres Tarantini <atarantini@gmail.com>
2015-08-13 01:58:35 -0300

    CLI argument parsing. New arguments: --min, --max.

--- dnscomb.py
+++ dnscomb.py
@@ -3,8 +3,12 @@ I want to build my own domain list by using a wordlist over a DNS server!
 """
 import socket
 import string
+import argparse
 
 import wordlist
+
+TLD = ".com.ar"
+MASK = "{name}{tld}"
 
 
 def domain_exists(hostname):
@@ -17,14 +21,18 @@ def domain_exists(hostname):
     return False
 
 
-TLD = ".com.ar"
-MASK = "{name}{tld}"
+if __name__ == "__main__":
+    # Parse CLI arguments
+    parser = argparse.ArgumentParser(description='dnscomb will build my own list of domains!')
+    parser.add_argument('--max', type=int, help='Maximum length of the domain name', default=3)
+    parser.add_argument('--min', type=int, help='Minimum length of the domain name', default=1)
+    args = parser.parse_args()
 
-generator = wordlist.Generator(string.ascii_lowercase)
-for name in generator.generate(1, 3):
-    hostname = MASK.format(
-        name=name,
-        tld=TLD
-    )
-    if domain_exists(hostname):
-        print hostname
+    generator = wordlist.Generator(string.ascii_lowercase)
+    for name in generator.generate(args.min, args.max):
+        hostname = MASK.format(
+            name=name,
+            tld=TLD
+        )
+        if domain_exists(hostname):
+            print hostname
